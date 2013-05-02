d7717cc71c4a73487e4a8977867f010cd2ea097f
xas <afchacon2@gmail.com>
2013-05-02 12:42:22 -0400

    initia commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mapper::Application.config.secret_token = 'e04da2f8986419e64f8220829b44068363786ea06343ac1bc2dfd131b66d1d9e661936b51f149032fe06d4c3f60416f76de28f075900c9d3a93f36dd3efdbd10'
