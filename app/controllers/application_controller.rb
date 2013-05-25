fd98d18902789d1a9a0bc9b7ceb7b1b23d61aa21
xas <afchacon2@gmail.com>
2013-05-25 11:56:08 -0400

    minor modifications to logged user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,15 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
 
-  before_filter :check_token, except: [:new] 
+  before_filter :check_token 
 
   protected
 
   def check_token
     if(session[:token]!=nil)
-      user = User.find_by_token(session[:token])
+      @logged_user = User.find_by_token(session[:token])
 
-      if(user==nil)
+      if(@logged_user==nil)
         session[:original_url] = request.url
         redirect_to :login
       end
