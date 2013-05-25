9c626cc353ab61dc2dfbf0b532e83789474c7745
Juan Antonio Karmy <jakarmy@gmail.com>
2013-05-25 12:14:38 -0400

    Fixing bugs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
 	def getCurrentUser
-    if session[:user_id]
-      return User.find(session[:user_id])
-  elsif params[:api_key]
-    return User.find_by_api_key(params[:api_key]) 
-  else
-    return false
-  end
+    	if session[:user_id]
+      	   return User.find(session[:user_id])
+  		elsif params[:api_key]
+    	   return User.find_by_api_key(params[:api_key]) 
+  		else
+    	   return false
+  		end
+	end
 end
