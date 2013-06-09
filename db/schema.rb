cc96b5872c9fbc11e3b8dfb58bcc307cdbc65beb
Juan Antonio Karmy <jakarmy@gmail.com>
2013-06-09 15:36:24 -0400

    Added Image table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130609172806) do
+ActiveRecord::Schema.define(:version => 20130609193557) do
+
+  create_table "images", :force => true do |t|
+    t.string   "url"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "places", :force => true do |t|
     t.string   "name"
