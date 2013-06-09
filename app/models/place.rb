56b2d6772bd27d78683fa2515874ef15de9621f0
Juan Antonio Karmy <jakarmy@gmail.com>
2013-06-09 16:04:20 -0400

    Added image table

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,9 @@
 class Place < ActiveRecord::Base
   attr_accessible :address, :gmaps, :latitude, :longitude, :name, :image
   has_and_belongs_to_many :trips
+  has_many :images
 
-  #acts_as_gmappable
+  acts_as_gmappable
 
 	def gmaps4rails_address
   		address
