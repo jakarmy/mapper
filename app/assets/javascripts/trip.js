e1264ac74c01cd3febfc4622af67711b7d2c65df
Juan Antonio Karmy <jakarmy@gmail.com>
2013-07-01 18:24:20 -0400

    first steps on search

--- app/assets/javascripts/trip.js
+++ app/assets/javascripts/trip.js
@@ -28,12 +28,12 @@ function searchPlaces(){
 	}
 }
 
-function searchPlacesCallback(data){
+// function searchPlacesCallback(data){
 
-	for (var i = 0; i < data.length; i++) {
-		console.log(data[i].name);
-	};
-}
+// 	for (var i = 0; i < data.length; i++) {
+// 		console.log(data[i].name);
+// 	};
+// }
 
 function textEnteredCallback(data){
 	
